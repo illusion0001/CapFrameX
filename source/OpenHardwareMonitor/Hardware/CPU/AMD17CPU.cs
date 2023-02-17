b1bff74e95ade05cf6e46cdea1255a4039bbcea3
DevTechProfile <devtechprofile@hotmail.com>
2023-02-17 14:44:57 +0100

    Ryzen SMU

--- source/OpenHardwareMonitor/Hardware/CPU/AMD17CPU.cs
+++ source/OpenHardwareMonitor/Hardware/CPU/AMD17CPU.cs
@@ -90,13 +90,13 @@ namespace OpenHardwareMonitor.Hardware.CPU
                 }
             }
 
-            //this.smu = new RyzenSMU(family, model, packageType);
-            //var pmTable = smu.GetPmTable();
+            this.smu = new RyzenSMU(family, model, packageType);
+            var pmTable = smu.GetPmTable();
 
-            //foreach (KeyValuePair<uint, RyzenSMU.SmuSensorType> sensor in smu.GetPmTableStructure())
-            //{
-            //    //_smuSensors.Add(sensor, new Sensor(sensor.Value.Name, _cpu._sensorTypeIndex[sensor.Value.Type]++, sensor.Value.Type, _cpu, _cpu._settings));
-            //}
+            foreach (KeyValuePair<uint, RyzenSMU.SmuSensorType> sensor in smu.GetPmTableStructure())
+            {
+                //_smuSensors.Add(sensor, new Sensor(sensor.Value.Name, _cpu._sensorTypeIndex[sensor.Value.Type]++, sensor.Value.Type, _cpu, _cpu._settings));
+            }
 
             coreTemperature = new Sensor(
               "CPU Package", 0, SensorType.Temperature, this, new[] {
